9cb3bb3a2acc190d24149d6e07bc873ff8034d11
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-01-22 13:42:27 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split('\n')
+    .map((s) => s.trim())
+    .filter((s) => s && s.includes(':'))
+    .map((s) => {
+      const [key, ...rest] = s.split(':');
+
+      let value = rest.join(':').trim();
+
+      value = value.replace(/;$/, '').trim();
+
+      return [key.trim(), value];
+    })
+    .reduce((styles, [key, value]) => {
+      styles[key] = value;
+
+      return styles;
+    }, {});
 }
 
 module.exports = convertToObject;
+
+// .filter(s => s !=='') s - це елемент styles[i]
